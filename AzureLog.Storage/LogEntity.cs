4bb5abeba54d5d54d6360b6c3524d8c40b1b2bdd
Soe Moe <soemoetunlwin@gmail.com>
2014-10-16 18:22:29 +0800

    add AzureLog.Storage

---
+++ AzureLog.Storage/LogEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureLog.Storage
+{
+    public class LogEntity : TableEntity
+    {
+        public LogEntity(string loggerName, string level, string message, DateTime logTimeStamp)
+        {
+            if (logTimeStamp.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException("logTimeStamp must be UTC.", "logTimeStamp");
+            }
+
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var unixTime = Convert.ToInt64((logTimeStamp - epoch).TotalSeconds);
+            RowKey = unixTime.ToString(CultureInfo.InvariantCulture);
+
+            var prefix = Convert.ToInt32(logTimeStamp.Ticks % 5);
+            PartitionKey =  string.Format("{0}-{1:u}", prefix, logTimeStamp);
+
+            LogTimeStamp = LogTimeStamp;
+            LoggerName = loggerName;
+            Level = level;
+            Message = message;
+        }
+
+        public LogEntity()
+        {
+
+        }
+
+        public string LogTimeStamp { get; set; }
+        public string Level { get; set; }
+        public string LoggerName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ AzureLog.Storage/LogTableClient.cs
@@ -0,0 +1,60 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureLog.Storage
+{
+    public class LogTableClient
+    {
+        private readonly CloudTable _cloudTable;
+        private readonly string _connectionStringKey;
+
+        private LogTableClient(string connectionStringKey, CloudTable cloudTable)
+        {
+            _connectionStringKey = connectionStringKey;
+            _cloudTable = cloudTable;
+        }
+
+        public static async Task<LogTableClient> CreateAsync(string connectionStringKey, string tableName)
+        {
+            var storageAccount = GetStorageAccount(connectionStringKey);
+            // Create the table client.
+            var tableClient = storageAccount.CreateCloudTableClient();
+            //create charts table if not exists.
+            var cloudTable = tableClient.GetTableReference(tableName);
+            await cloudTable.CreateIfNotExistsAsync();
+
+            return new LogTableClient(connectionStringKey, cloudTable);
+        }
+
+        private static string GetStorageAccountConnectionString(string connectionStringKey)
+        {
+            // try get connection string from app settings or could service config
+            var connectionStringValue = CloudConfigurationManager.GetSetting(connectionStringKey);
+            if (!string.IsNullOrEmpty(connectionStringValue)) return connectionStringValue;
+
+            // try get connection string from ConfigurationManager.ConnectionStrings
+            var connectionString = ConfigurationManager.ConnectionStrings[connectionStringKey];
+            if (connectionString != null)
+            {
+                connectionStringValue = connectionString.ConnectionString;
+            }
+            return connectionStringValue;
+        }
+
+        private static CloudStorageAccount GetStorageAccount(string connectionStringKey)
+        {
+            var connectionString = GetStorageAccountConnectionString(connectionStringKey);
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            return storageAccount;
+        }
+
+        public async Task InsertAsync(LogEntity entity)
+        {
+            var insertOperation = TableOperation.Insert(entity);
+            await _cloudTable.ExecuteAsync(insertOperation);
+        }
+    }
+}
