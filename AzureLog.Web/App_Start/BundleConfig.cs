1799b69d15cb7d7ee33dd8feb8534f51617e6ba4
Soe Moe <soemoetunlwin@gmail.com>
2014-10-17 19:01:32 +0800

    add Flatly theme and change UI for home page

--- AzureLog.Web/App_Start/BundleConfig.cs
+++ AzureLog.Web/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace AzureLog.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.flatly.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
