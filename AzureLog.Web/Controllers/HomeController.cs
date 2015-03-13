1f19960b4d0e0a59c51bb7a0b711c2c86b665b1f
Soe Moe <soemoetunlwin@gmail.com>
2015-03-13 15:02:02 +0630

    remove contact and about

--- AzureLog.Web/Controllers/HomeController.cs
+++ AzureLog.Web/Controllers/HomeController.cs
@@ -105,19 +105,5 @@ namespace AzureLog.Web.Controllers
 
             return result;
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
